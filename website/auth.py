6c6a82730d8da82d8ee0c39ea6bada84d1823cac
AiRa1909 <safa.rasyidin@thekaustschool.org>
2025-11-25 09:59:26 +0300

    Day 3 (25th of November)

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, flash
+
+from main import read_temperature
 
 auth = Blueprint('auth', __name__)
 
+@app.route("/temperature")
+def temperature():
+    c, f = read_temperature()
+    if c > 75:
+        flash("TOO HOTT!!!")
+    else:
+        pass
+
+    return render_template("page.html",c=c,f=f)
