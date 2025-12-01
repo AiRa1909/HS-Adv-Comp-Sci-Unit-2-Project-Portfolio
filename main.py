da022e9869c0af06fded15bd3937d473246269f2
AiRa1909 <safa.rasyidin@thekaustschool.org>
2025-12-01 09:17:05 +0300

    Day 4 (1st of December)

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from website import create_app
+from meteostat import Point, Hourly
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-def read_temperature():
-    path = #i will write path here later i dont know what folder the temperature values will be in right now
-
-    with open(path, "r") as f:
-        lines = f.readlines()
-
-    temp_str = lines[1].split("t=")[1]
-
-    c = float(temp_str) / 1000
-    f = (c * 9 / 5) + 32
-
-    return c, f
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,2 @@
-from flask import Blueprint, render_template, flash
-
-from main import read_temperature
-
+from flask import Blueprint
 auth = Blueprint('auth', __name__)
-
-@app.route("/temperature")
-def temperature():
-    c, f = read_temperature()
-    if c > 75:
-        flash("TOO HOTT!!!")
-    else:
-        pass
-
-    return render_template("page.html",c=c,f=f)
