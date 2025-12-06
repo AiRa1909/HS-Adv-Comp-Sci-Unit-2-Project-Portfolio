f9de11220e767521d1169ed6935970c2da0da868
AiRa1909 <safa.rasyidin@thekaustschool.org>
2025-12-06 19:05:04 +0300

    Final FINAL Code (Dec 6)

--- website/auth.py
+++ website/auth.py
@@ -5,15 +5,21 @@ auth = Blueprint('auth', __name__)
 
 @auth.route("/")
 def temperature():
+    #defines the view function that is called when the user accesses the default route "/" when they click the link
     c = float(read_temperature())
-    if 25 < c and 28 > c:
-        flash("WARNING! Heatwave Temperatures Reached")
+    #gets the celsius value from stuff.py
+    #converts it to a float so that it can be calculated into f
+    if 25 < c and 40 > c:
+        flash("WARNING! Heatwave Temperatures Reached", "error")
+        #uses flask flash SMS that sends this message to the html template
     elif c > 40:
-        flash("WARNING! Highest Heatwave Temperatures Reached!")
+        flash("WARNING! Highest Heatwave Temperatures Reached!", "error")
     else:
         pass
 
-    f = c * 9 / 5 + 32
+    f = round(c * 9 / 5 + 32, 1)
+    #follows standard formula to convert celsius to fahrenheit
 
     return render_template("base.html", c=c, f=f)
+#rendering the base.html template to display the web page to the user, while passing c and f to the template as parameters
 
--- website/stuff.py
+++ website/stuff.py
@@ -2,21 +2,29 @@ import requests
 
 def read_temperature():
     url = "https://api.open-meteo.com/v1/forecast"
+    # open access url Open Meteo provides themselves
     params = {
         "latitude": 52.9540,
         "longitude": 1.1550,
         "current_weather": True
     }
+    # a dictionary that contains all the parameters we need to give in the HTTP route to open meteo
     r = requests.get(url, params=params)
+    # a GET HTTP request to the Open Meteo URL with the parameters
     data = r.json()
+    # parse the JSON content of the HTTP response  and convert it into a Python dictionary
     temp_c = str(data["current_weather"]["temperature"])
+    #FIND the temperature value-pair that contains the celsius temperature IN the python dictionary "current_weather"
 
     try:
         with open("sensor.txt", "w") as f:
             f.write(temp_c)
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+    #displays correct use of the try-except block to except any errors
+    #writes to file
 
     with open("sensor.txt", "r") as f:
         celsius = f.read()
         return celsius
+    #reads from file
